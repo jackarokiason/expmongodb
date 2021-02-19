df4fcb2996a09b3a6e8b12a1a149cdc10e99551a
Jack Arokiason <jackjohnson2220000@gmail.com>
2021-02-19 18:41:57 +0530

    updated

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ const assert = require('assert');
 const url = 'mongodb://localhost:27017';
 
 // Database Name
-const dbName = 'myproject';
+const dbName = 'IplAuctionDB';
 
 // Create a new MongoClient
-const client = new MongoClient(url);
+const client = new MongoClient(url, { useUnifiedTopology: true });
 
 // Use connect method to connect to the Server
 client.connect(function(err) {
@@ -16,6 +16,36 @@ client.connect(function(err) {
   console.log("Connected successfully to server");
 
   const db = client.db(dbName);
-
-  client.close();
+  insertDocuments(db, function(){
+    client.close();
+  });
+  
 });
+
+const insertDocuments = function(db, callback) {
+  // Get the documents collection
+  const collection = db.collection('ipl');
+  // Insert some documents
+  collection.insertMany([
+    {Name: "Maxwell",
+     Bid: 14,
+     review:"Good batsman"
+
+  }, 
+    {Name: "Starc",
+    Bid: 14,
+    review:"Good bowler"
+  },
+
+    {Name: "Smith",
+    Bid: 14,
+    review:"Good batsman"
+  }
+  ], function(err, result) {
+    assert.equal(err, null);
+    assert.equal(3, result.result.n);
+    assert.equal(3, result.ops.length);
+    console.log("Inserted 3 documents into the collection");
+    callback(result);
+  });
+}
